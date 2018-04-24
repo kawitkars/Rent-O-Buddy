09b4157d7bf03a8a7043efe4a0a5686a853935d2
Sneha Kawitkar <kawitkar.s@husky.neu.edu>
2018-04-24 03:38:41 -0400

    MongoDB added

---
+++ RentoBuddy/mvc6-mongodb-webapi-master/Models/OrderModel.cs
@@ -0,0 +1,43 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC6_WEBAPI_MongoDB.Models
+{
+    public class OrderModel
+    {
+        [BsonId]
+        public ObjectId _id { get; set; }
+
+        public int OrderId { get; set; }
+
+        public CustomerModel CustomerModel { get; set; }
+
+        public PaymentModel PaymentModel { get; set; }
+
+        public List<OrderProductModel> OrderProductModel { get; set; }
+
+        public double TotalRentalDeposit { get; set; }
+
+        public double TotalRentAmount { get; set; }
+
+        public double TotalCostForOrder { get; set; }
+
+        public double TaxesApplied { get; set; }
+
+        public string DiscountCode { get; set; }
+
+        public double DiscountApplied { get; set; }
+
+        public OrderModel()
+        {
+            CustomerModel = new CustomerModel();
+            PaymentModel = new PaymentModel();
+            OrderProductModel = new List<OrderProductModel>();
+        }
+    }
+}
+   
---
+++ RentoBuddy/mvc6-mongodb-webapi-master/Models/PaymentModel.cs
@@ -0,0 +1,33 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC6_WEBAPI_MongoDB.Models
+{
+    public class PaymentModel
+    {
+        [BsonId]
+        public ObjectId _id { get; set; }
+
+        [BsonElement("PaymentId")]
+        public int PaymentId { get; set; }
+
+        [BsonElement("PaymentMode")]
+        public string PaymentMode { get; set; }
+
+        [BsonElement("CardNumber")]
+        public string CardNumber { get; set; }
+
+        [BsonElement("ExpirationDate")]
+        public string ExpirationDate { get; set; }
+
+        [BsonElement("CVVNumber")]
+        public string CVVNumber { get; set; }
+
+        [BsonElement("NameOnCard")]
+        public string NameOnCard { get; set; }
+    }
+}
